a4b3acc93ff7cace0d9430e062512dffad9b3677
Fauzan Rabbani <jhanojan@gmail.com>
2021-10-07 11:45:10 +0700

    Update smp.php

--- application/views/contents/template_report/smp.php
+++ application/views/contents/template_report/smp.php
@@ -21,77 +21,77 @@ if(empty($nilai)){
 </table>
 <table width="100%" border="1" cellspacing='0'>
     <tr>
-        <td>Mata Pelajaran</td>
-        <td>Presensi</td>
-        <td>Bahan Ajar</td>
-        <td>Nilai Pengetahuan</td>
-        <td>Nilai Keterampilan</td>
+        <td><b>Mata Pelajaran</b></td>
+        <td><b>Presensi</b></td>
+        <td><b>Bahan Ajar</b></td>
+        <td><b>Nilai Pengetahuan</b></td>
+        <td><b>Nilai Keterampilan</b></td>
     </tr>
     <tr>
-      <td class="ctr">PAI</td>
+        <td class="ctr"><b>PAI</b></td>
       <td class="ctr"><?php echo $nilai['pai_presensi']?></td>
       <td class="ctr"><?php echo $nilai['pai_ba']?></td>
       <td class="ctr"><?php echo $nilai['pai_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['pai_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">PPKn</td>
+      <td class="ctr"><b>PPKn</b></td>
       <td class="ctr"><?php echo $nilai['pkn_presensi']?></td>
       <td class="ctr"><?php echo $nilai['pkn_ba']?></td>
       <td class="ctr"><?php echo $nilai['pkn_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['pkn_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">Bahasa Indonesia</td>
+      <td class="ctr"><b>Bahasa Indonesia</b></td>
       <td class="ctr"><?php echo $nilai['bi_presensi']?></td>
       <td class="ctr"><?php echo $nilai['bi_ba']?></td>
       <td class="ctr"><?php echo $nilai['bi_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['bi_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">Matematika</td>
+      <td class="ctr"><b>Matematika</b></td>
       <td class="ctr"><?php echo $nilai['mtk_presensi']?></td>
       <td class="ctr"><?php echo $nilai['mtk_ba']?></td>
       <td class="ctr"><?php echo $nilai['mtk_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['mtk_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">IPA</td>
+      <td class="ctr"><b>IPA</b></td>
       <td class="ctr"><?php echo $nilai['ipa_presensi']?></td>
       <td class="ctr"><?php echo $nilai['ipa_ba']?></td>
       <td class="ctr"><?php echo $nilai['ipa_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['ipa_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">IPS</td>
+      <td class="ctr"><b>IPS</b></td>
       <td class="ctr"><?php echo $nilai['ips_presensi']?></td>
       <td class="ctr"><?php echo $nilai['ips_ba']?></td>
       <td class="ctr"><?php echo $nilai['ips_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['ips_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">Bahasa Inggris</td>
+      <td class="ctr"><b>Bahasa Inggris</b></td>
       <td class="ctr"><?php echo $nilai['en_presensi']?></td>
       <td class="ctr"><?php echo $nilai['en_ba']?></td>
       <td class="ctr"><?php echo $nilai['en_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['en_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">PJOK</td>
+      <td class="ctr"><b>PJOK</b></td>
       <td class="ctr"><?php echo $nilai['pjok_presensi']?></td>
       <td class="ctr"><?php echo $nilai['pjok_ba']?></td>
       <td class="ctr"><?php echo $nilai['pjok_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['pjok_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">SB</td>
+      <td class="ctr"><b>SB</b></td>
       <td class="ctr"><?php echo $nilai['sb_presensi']?></td>
       <td class="ctr"><?php echo $nilai['sb_ba']?></td>
       <td class="ctr"><?php echo $nilai['sb_pengetahuan']?></td>
       <td class="ctr"><?php echo $nilai['sb_keterampilan']?></td>
     </tr>
     <tr>
-      <td class="ctr">Informatika</td>
+      <td class="ctr"><b>Informatika</b></td>
       <td class="ctr"><?php echo $nilai['tik_presensi']?></td>
       <td class="ctr"><?php echo $nilai['tik_ba']?></td>
       <td class="ctr"><?php echo $nilai['tik_pengetahuan']?></td>
@@ -101,10 +101,10 @@ if(empty($nilai)){
 <br/>
 <table width="100%" border="1" cellspacing='0'>
     <tr>
-        <td class="ctr" rowspan="2">BBQ</td>
-        <td class="ctr">Presensi</td>
-        <td class="ctr">Jilid</td>
-        <td class="ctr">Nilai</td>
+        <td class="ctr" rowspan="2"><b>BBQ</td>
+        <td class="ctr"><b>Presensi</b></td>
+        <td class="ctr"><b>Jilid</b></td>
+        <td class="ctr"><b>Nilai</b></td>
     </tr>
     <tr>
         <td class="ctr"><?php echo $nilai['bbq_presensi']?></td>
